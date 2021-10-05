0f47fb1c3eee59724824d7334180a335bb4f0849
MrJooka <mr.jooka@gmail.com>
2021-10-06 08:36:44 +0900

    테스트 - 잘못된 상수 설정

--- test/test.js
+++ test/test.js
@@ -3,12 +3,11 @@ const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
 describe('province', function () {
+  const asia = new Province(sampleProvinceData()); // 잘못 된 테스트 방법
   it('shortfall', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
   it('profit', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(20);
   });
 });
